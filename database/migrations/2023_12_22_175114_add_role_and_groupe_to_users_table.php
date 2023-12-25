14462a57ac2dde90874c33c240fd2843fc22a615
oubellaismail <i.oubella1739@uca.ac.ma>
2023-12-25 10:19:47 +0100

    add users foreign key !

---
+++ database/migrations/2023_12_22_175114_add_role_and_groupe_to_users_table.php
@@ -0,0 +1,29 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('groupe_id')->nullable()->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropForeign(['groupe_id']);
+            $table->dropColumn(['role_id', 'groupe_id']);
+        });
+    }
+};
